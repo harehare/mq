f9e911f0e795a4010107c3743a124d407ea9f16d
harehare <harehare1110@gmail.com>
2025-03-26 21:43:37 +0900

    :white_check_mark: Update fuzz testing

--- fuzz/fuzz_targets/interpreter.rs
+++ fuzz/fuzz_targets/interpreter.rs
@@ -1,35 +1,64 @@
 #![no_main]
 
-use std::panic::catch_unwind;
-
 use arbitrary::Arbitrary;
+use itertools::Itertools;
 use libfuzzer_sys::fuzz_target;
 
 #[derive(Debug, Clone, Arbitrary)]
-struct Context<'a> {
-    script: &'a str,
+enum Expr {
+    Let(String, String),
+    Def(String, Vec<String>, String),
+    Call(String, Vec<String>),
+    Raw(String),
+}
+
+#[derive(Debug, Clone, Arbitrary)]
+struct ArbitraryScript {
+    exprs: Vec<Expr>,
+}
+
+impl ArbitraryScript {
+    fn to_script(&self) -> String {
+        let mut script = String::new();
+        for stmt in &self.exprs {
+            match stmt {
+                Expr::Let(name, value) => {
+                    script.push_str(&format!("let {} = {}\n", name, value));
+                }
+                Expr::Call(name, args) => {
+                    let args_str = args.join(", ");
+                    script.push_str(&format!("{}({})", name, args_str));
+                }
+                Expr::Def(name, args, body) => {
+                    let args_str = args.join(", ");
+                    script.push_str(&format!("def {}({}) {{ {} }};\n", name, args_str, body));
+                }
+                Expr::Raw(code) => {
+                    script.push_str(code);
+                    script.push('\n');
+                }
+            }
+        }
+        script
+    }
+}
+
+#[derive(Debug, Clone, Arbitrary)]
+struct Context {
+    raw_script: Option<String>,
+    generated_script: Option<Vec<ArbitraryScript>>,
 }
 
 fuzz_target!(|context: Context| {
-    let result = catch_unwind(|| {
-        let mut engine = mq_lang::Engine::default();
-        let _ = engine.eval(
-            context.script,
-            vec![mq_lang::Value::String("".to_string())].into_iter(),
-        );
-    });
+    let script = match (&context.raw_script, &context.generated_script) {
+        (Some(raw), _) => raw.clone(),
+        (_, Some(generated)) => generated.iter().map(|g| g.to_script()).join(" | "),
+        _ => "".to_string(),
+    };
 
-    match result {
-        Ok(_) => (),
-        Err(panic) => {
-            let is_stack_overflow = panic
-                .downcast_ref::<String>()
-                .map(|s| s.contains("stack-overflow"))
-                .unwrap_or(false);
-
-            if !is_stack_overflow {
-                std::panic::resume_unwind(panic);
-            }
-        }
-    }
+    let mut engine = mq_lang::Engine::default();
+    let _ = engine.eval(
+        &script,
+        vec![mq_lang::Value::String("".to_string())].into_iter(),
+    );
 });
