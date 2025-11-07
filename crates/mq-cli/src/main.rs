413c42cd29ca228b13e62ce444addd90b2bcb33d
harehare <harehare1110@gmail.com>
2025-11-07 23:25:15 +0900

    ✨ feat(cli): enable use of mimalloc as an optional global allocator

--- crates/mq-cli/src/main.rs
+++ crates/mq-cli/src/main.rs
@@ -1,7 +1,8 @@
+use clap::Parser;
+
+#[cfg(feature = "use_mimalloc")]
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
-
-use clap::Parser;
 
 fn main() -> miette::Result<()> {
     mq_cli::Cli::parse().run()
