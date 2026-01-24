1541e67f070c1bd8ba41a60a3f58397ec8c4d807
harehare <harehare1110@gmail.com>
2026-01-25 00:15:44 +0900

    ✨ chore: bump version to 0.5.12 and update gsub functionality in bump scripts

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export MQ_PREV_VERSION="0.5.10"
-export MQ_VERSION="0.5.11"
+export MQ_PREV_VERSION="0.5.11"
+export MQ_VERSION="0.5.12"
 export README="../README.md"
 export INSTALL_DOC="../docs/books/src/start/install.md"
 
@@ -31,21 +31,10 @@ tmpfile=$(mktemp)
 mq -I text --args version $MQ_VERSION 'import "bump_version" | bump_version::py_version()' "../crates/mq-python/pyproject.toml" > "$tmpfile" && mv "$tmpfile" "../crates/mq-python/pyproject.toml"
 
 # Update README.md with the new version
-mq -U --args VERSION $MQ_VERSION '.code | select(contains("docker")) | update(s"$$ docker run --rm ghcr.io/harehare/mq:${VERSION}")' $README > README.md.tmp \
+mq -U --args VERSION $MQ_VERSION 'import "bump_version" | bump_version::code_block_version(VERSION)' $README > README.md.tmp \
   && mv README.md.tmp $README
 
-mq -U --args VERSION $MQ_VERSION '.code | select(contains("docker")) | update(s"$$ docker run --rm ghcr.io/harehare/mq:${VERSION}")' $INSTALL_DOC > INSTALL_DOC.md.tmp \
-  && mv INSTALL_DOC.md.tmp $INSTALL_DOC
-
-mq -U --args VERSION $MQ_VERSION '.code | select(contains("cargo install --git https://github.com/harehare/mq.git mq-run")) | gsub("--tag.+", s"--tag v${VERSION}")' $README > README.md.tmp \
-  && mv README.md.tmp $README
-
-mq -U --args version $MQ_VERSION --args prev_version $MQ_PREV_VERSION '.code | select(contains("curl -L https://github.com/harehare/mq/releases/download/")) | gsub(s"v${prev_version}", s"v${version}")' $README > README.md.tmp \
-  && mv README.md.tmp $README
-
-mq -U --args VERSION $MQ_VERSION '.code | select(contains("cargo install --git https://github.com/harehare/mq.git mq-run")) | gsub("--tag.+", s"--tag v${VERSION}")' $INSTALL_DOC > INSTALL_DOC.md.tmp \
-  && mv INSTALL_DOC.md.tmp $INSTALL_DOC
-
-mq -U --args version $MQ_VERSION --args prev_version $MQ_PREV_VERSION '.code | select(contains("https://github.com/harehare/mq/releases/download/")) | gsub(s"v${prev_version}", s"v${version}")' $INSTALL_DOC > INSTALL_DOC.md.tmp \
+# Update INSTALL_DOC.md with the new version
+mq -U --args VERSION $MQ_VERSION 'import "bump_version" | bump_version::code_block_version(VERSION)' $INSTALL_DOC > INSTALL_DOC.md.tmp \
   && mv INSTALL_DOC.md.tmp $INSTALL_DOC
 
