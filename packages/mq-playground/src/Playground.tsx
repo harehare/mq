1a99454da95e3a9239933a8408dbb76b5c9ebe09
harehare <harehare1110@gmail.com>
2026-06-10 09:35:42 +0900

    ♻️ refactor(playground): remove unused useMemo import

--- packages/mq-playground/src/Playground.tsx
+++ packages/mq-playground/src/Playground.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback, useRef, useMemo } from "react";
+import { useState, useEffect, useCallback, useRef } from "react";
 import Editor, { Monaco } from "@monaco-editor/react";
 import "./index.css";
 import "./vim.css";
